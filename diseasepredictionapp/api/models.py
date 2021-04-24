f0aced3f02591f89e2834f2f2117757d800d4346
Tuna Aybar Tas <aybartass@gmail.com>
2021-04-24 18:33:09 +0300

    db changes

--- diseasepredictionapp/api/models.py
+++ diseasepredictionapp/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+import string
+import random
 # Create your models here.
+
+class Disease(models.Model):
+    
+    id = models.IntegerField(default="",unique=True,primary_key=True)
+    name = models.CharField(max_length=100,default="")
+
+
+class Record(models.Model):
+    
+    id = models.IntegerField(max_length=10,default="", unique=True,primary_key=True)
+    feature_1 = models.CharField(max_length=50,default="")
+    feature_2 = models.CharField(max_length=50,default="")
+    feature_3 = models.CharField(max_length=50,default="")
+    predicted_disease = models.CharField(max_length=50,default="")
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+
