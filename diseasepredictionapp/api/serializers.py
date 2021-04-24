207e6c2d2ddfc492df4b85b43875717cd8d41e77
Tuna Aybar Tas <aybartass@gmail.com>
2021-04-24 23:50:06 +0300

    Revert "Migration completed"
    This reverts commit e50619682d7993fd6542b05f03c137c7ced8b356.

--- diseasepredictionapp/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Disease
-from rest_framework import serializers
-
-
-class DiseaseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Disease
-        fields = ('id', 'name')
