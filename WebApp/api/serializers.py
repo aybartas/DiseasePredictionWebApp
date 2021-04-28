f3acf40f5a439d97cf9511229ef9e79f689abdb8
Tuna Aybar Tas <aybartass@gmail.com>
2021-04-28 03:03:00 +0300

    Post done / bug fixed

--- WebApp/api/serializers.py
+++ WebApp/api/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from .models import Disease
 
 
+# Serializer for responding client
 class DiseaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Disease
         fields = ('id', 'name')
+
+
+# Serializer that will handle post request and get data from payload to return something
+class CreateDiseaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disease
+        fields = 'name'
--- WebApp/api/views.py
+++ WebApp/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from .models import Disease
-from .serializers import DiseaseSerializer
+from .serializers import DiseaseSerializer, CreateDiseaseSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -11,3 +11,18 @@ from rest_framework.response import Response
 class DiseaseView(generics.ListAPIView):
     queryset = Disease.objects.all()
     serializer_class = DiseaseSerializer
+
+
+class CreateDiseaseView(APIView):
+    serializer_class = CreateDiseaseSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            disease_name = serializer.data.name
+            queryset = Disease.objects.filter(name=disease_name)
+            if queryset.exists():
+                print('disease already exists')
+            else:
+                disease = Disease(name=disease_name)
+            return Response(DiseaseSerializer(disease).data, status=status.HTTP_200_OK)
