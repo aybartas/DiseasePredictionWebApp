1f90ebbce43018ccd6cfef91d7b8dabbbd638f40
Tuna Aybar Tas <aybartass@gmail.com>
2021-05-01 21:27:48 +0300

    form refactored

--- WebApp/frontend/src/components/Prediction.js
+++ WebApp/frontend/src/components/Prediction.js
@@ -1,92 +1,169 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import {
-    FormControl,
     FormHelperText,
     Input,
-    InputLabel,
     Grid,
     Typography,
     RadioGroup,
-    FormControlLabel, FormLabel, Radio, Checkbox, TextField, Button, makeStyles
+    makeStyles,
+    Card,
+    CardContent,
+    MenuItem,
+    InputLabel,
+    Select,
+    CardActions,
+    CardHeader,
+    FormControl,
+    FormControlLabel, FormLabel, Radio, Checkbox, Button
 } from "@material-ui/core";
 
-import { ValidatorForm, TextValidator,SelectValidator} from 'react-material-ui-form-validator';
+import {Formik, Form, Field} from "formik"
+import * as Yup from "yup"
+import {TextField} from "formik-material-ui"
 
-export default class Prediction extends Component{
+const useStyle = makeStyles((theme) => ({
+    padding: {
+        padding: theme.spacing(3),
+    },
+    button: {
+        margin: theme.spacing(1)
+    },
 
-    state = {
-        formData: {
-            feature_1: '',
-            feature_2:''
-        },
-        submitted: false,
-    }
+}));
 
-    handleChange = (event) => {
+const options = [
+    {label: "Kadın", value: "Kadın"},
+    {label: "Erkek", value: "Erkek"}
+]
 
-        const { formData } = this.state;
-        formData[event.target.name] = event.target.value;
-        this.setState({ formData });
-    }
+let validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("Required"),
+    /*
+  password: Yup.string()
+    .matches(
+      lowercaseRegEx,
+      "Must contain one lowercase alphabetical character!"
+    )
+    .matches(
+      uppercaseRegEx,
+      "Must contain one uppercase alphabetical character!"
+    )
+    .matches(numericRegEx, "Must contain one numeric character!")
+    .required("Required!"),
+    */
+})
 
-    handleSubmit = () => {
-        this.setState({ submitted: true }, () => {
-            setTimeout(() => this.setState({ submitted: false }), 3000);
-        });
+const Prediction = () => {
 
-        const requestOptions = {
-            method:'POST',
-            headers:{'Content-Type':'application/json'},
-            body:JSON.stringify({
+    const classes = useStyle()
 
-                feature_1: this.state.formData.feature_1,
-                feature_2: this.state.formData.feature_2
+    const onSubmit = (values) => {
+
+         const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+
+                feature_1: values.firstName,
+                feature_2: values.occupation
             })
         };
 
-        fetch('/api/addRecord',requestOptions)
+        fetch('/api/addRecord', requestOptions)
             .then((response) => response.json())
             .then((data) => console.log(data));
-
     }
 
-    render() {
-        const { formData, submitted } = this.state;
-        return (
-            <ValidatorForm
+    return (
+        <Grid container justify="center" spacing={1}>
+            <Grid item md={6}>
+                <Card className={classes.padding}>
+                    <CardHeader title="PREDICTION INPUT FORM"/>
+                    <Formik
+                        initialValues={{firstName: "", occupation: ""}}
+                        validationSchema={validationSchema}
+                        onSubmit={onSubmit}>
+                        {({dirty, isValid, values, handleChange,handleReset}) => {
+                            return (
+                                <Form>
+                                    <CardContent>
+                                        <Grid item container spacing={1} justify="center">
+                                            <Grid item xs={12} sm={6} md={6}>
+                                                <Field
+                                                    label="First Name"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    name="firstName"
+                                                    value={values.firstName ? values.firstName : ""}
+                                                    component={TextField}
+                                                />
+                                            </Grid>
 
-                ref="form"
-                onSubmit={this.handleSubmit}>
-                <Typography variant="h5" gutterBottom>
-                     Prediction
-                </Typography>
-                <TextValidator
-                    label="Feature 1"
-                    onChange={this.handleChange}
-                    name="feature_1"
-                    value={formData.feature_1}
-                    validators={['required']}
-                    errorMessages={['Fields can not be empty']}
-                />
+                                            <Grid item xs={12} sm={6} md={12}>
+                                                <FormControl fullWidth variant="outlined">
+                                                    <InputLabel id="demo-simple-select-outlined-label">
+                                                        Occupation
+                                                    </InputLabel>
+                                                    <Select
+                                                        labelId="demo-simple-select-outlined-label"
+                                                        id="demo-simple-select-outlined"
+                                                        label="Occupation"
+                                                        onChange={handleChange}
+                                                        value={values.occupation ? values.occupation :""}
+                                                        name="occupation">
+                                                        <MenuItem>None</MenuItem>
+                                                        {options.map((item) => (
+                                                            <MenuItem key={item.value} value={item.value}>
+                                                                {item.label}
+                                                            </MenuItem>
+                                                        ))}
+                                                    </Select>
+                                                </FormControl>
+                                            </Grid>
+                                            <Grid item xs={12} sm={6} md={6}>
+                                                <Field
+                                                    label="City"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    name="city"
+                                                    value={values.city ? values.city :""}
+                                                    component={TextField}
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12} sm={6} md={6}>
+                                                <Field
+                                                    label="Country"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    name="country"
+                                                    value={values.country ? values.country:""}
+                                                    component={TextField}
+                                                />
+                                            </Grid>
 
-                <RadioGroup aria-label="feature_2" name="feature_2"  onChange={this.handleChange}>
-                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="no" control={<Radio />} label="No" />
+                                        </Grid>
+                                    </CardContent>
 
-              </RadioGroup>
+                                    <CardActions>
+                                        <Button
 
-                <br />
-                <Button
-                    color="primary"
-                    variant="contained"
-                    type="submit"
-                    disabled={submitted}>
-                    {
-                        (submitted && 'Form submitted') || (!submitted && 'Submit')
-                    }
-                </Button>
+                                            disabled={!dirty || !isValid}
+                                            variant="outlined"
+                                            color="primary"
+                                            type="Submit"
+                                            className={classes.button}>
+                                            Predict
 
-            </ValidatorForm>
-        );
-    }
+                                        </Button>
+                                    </CardActions>
+                                </Form>
+                            )
+                        }}
+                    </Formik>
+                </Card>
+            </Grid>
+        </Grid>
+    )
 }
+
+export default Prediction
